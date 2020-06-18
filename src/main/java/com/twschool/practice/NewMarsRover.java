1618dda9e9a8969ba28a8048cae6b3ff2997f89a
yangjie <yangjienice@126.com>
2020-06-19 00:11:47 +0800

    完成左转

--- src/main/java/com/twschool/practice/NewMarsRover.java
+++ src/main/java/com/twschool/practice/NewMarsRover.java
@@ -3,6 +3,7 @@ package com.twschool.practice;
 public class NewMarsRover {
 
     private NewMarsRoverPosition newMarsRoverPosition = new NewMarsRoverPosition();
+    private String directions = "NWSE";
 
     public NewMarsRover(int x, int y, String direction){
         newMarsRoverPosition.setDirection(direction);
@@ -14,6 +15,25 @@ public class NewMarsRover {
         if(command.length() < 1){
             return newMarsRoverPosition;
         }
+        if(command.equals("L")){
+            return turnLeft(newMarsRoverPosition);
+        }
         return newMarsRoverPosition;
+    }
+
+    private NewMarsRoverPosition turnLeft(NewMarsRoverPosition position) {
+        String dir = position.getDirection();
+        int index = directions.indexOf(dir);
+        switch(index){
+            case 0:
+            case 1:
+            case 2:
+                position.setDirection(String.valueOf(directions.charAt(index+1)));
+                break;
+            case 3:
+                position.setDirection(String.valueOf(directions.indexOf(0)));
+                break;
+        }
+        return position;
     }
 }
--- src/test/java/com/twschool/practice/NewMarsRoverTest.java
+++ src/test/java/com/twschool/practice/NewMarsRoverTest.java
@@ -15,4 +15,19 @@ public class NewMarsRoverTest {
         Assert.assertEquals(0,position.getY());
         Assert.assertEquals("N",position.getDirection());
     }
+
+    @Test
+    public void should_return_x_0_y_0_direction_W_when_given_x_0_y_0_direction_N(){
+        //given
+        NewMarsRover newMarsRover = new NewMarsRover(0,0,"N");
+        //when
+        NewMarsRoverPosition position = newMarsRover.exeute("L");
+        //then
+        Assert.assertEquals(0,position.getX());
+        Assert.assertEquals(0,position.getY());
+        Assert.assertEquals("W",position.getDirection());
+    }
+
+
+
 }
