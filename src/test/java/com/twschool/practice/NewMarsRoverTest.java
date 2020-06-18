6f1a58a735eed9bdac0f58c4f0d2d45e1ce5ed7a
yangjie <yangjienice@126.com>
2020-06-19 00:18:36 +0800

    补充左转用例

--- src/test/java/com/twschool/practice/NewMarsRoverTest.java
+++ src/test/java/com/twschool/practice/NewMarsRoverTest.java
@@ -28,6 +28,41 @@ public class NewMarsRoverTest {
         Assert.assertEquals("W",position.getDirection());
     }
 
+    @Test
+    public void should_return_x_0_y_0_direction_S_when_given_x_0_y_0_direction_W(){
+        //given
+        NewMarsRover newMarsRover = new NewMarsRover(0,0,"W");
+        //when
+        NewMarsRoverPosition position = newMarsRover.exeute("L");
+        //then
+        Assert.assertEquals(0,position.getX());
+        Assert.assertEquals(0,position.getY());
+        Assert.assertEquals("S",position.getDirection());
+    }
+
+    @Test
+    public void should_return_x_0_y_0_direction_E_when_given_x_0_y_0_direction_S(){
+        //given
+        NewMarsRover newMarsRover = new NewMarsRover(0,0,"S");
+        //when
+        NewMarsRoverPosition position = newMarsRover.exeute("L");
+        //then
+        Assert.assertEquals(0,position.getX());
+        Assert.assertEquals(0,position.getY());
+        Assert.assertEquals("E",position.getDirection());
+    }
+
+    @Test
+    public void should_return_x_0_y_0_direction_N_when_given_x_0_y_0_direction_E(){
+        //given
+        NewMarsRover newMarsRover = new NewMarsRover(0,0,"E");
+        //when
+        NewMarsRoverPosition position = newMarsRover.exeute("L");
+        //then
+        Assert.assertEquals(0,position.getX());
+        Assert.assertEquals(0,position.getY());
+        Assert.assertEquals("N",position.getDirection());
+    }
 
 
 }
